541080a91a12cd8f2ec92ba3ffa63ee0612ef627
Abrahan <abrahanramirez2410@gmail.com>
2025-06-03 01:01:18 -0400

    Primeros pasos <print,variabes,funcion

---
+++ aprendiendo.py
@@ -0,0 +1,24 @@
+#Imprimir en consola
+print("hola abran")
+
+#Asignar valores a las variables python entiende el tipo de dato como javascript
+a = 2
+b = 2
+print(a+b)
+
+#ejemplo condicional
+def condicional():
+    if a == 2:
+        print("Es 2")
+    else:
+        print("no es 2")
+
+#decimales
+decimal = 2.5
+#cadena de caracteres 
+nombre = "Abrahan"
+
+#imprimiendo ejemplos /Para imprimir una funcion se tiene que meter entre parentesis
+print(condicional())
+#
+print("El valor de nombre es: " + nombre)
